ca1adebbe98a647563cb4b65c8180e92a469c1a7
BulgaRpl <69915160+BulgaRpl@users.noreply.github.com>
2021-12-21 22:49:16 +0100

    Fix Shit

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
-game 'rdr3'
-fx_version 'adamant'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+fx_version "adamant"
 
-client_scripts {
-  'client.lua'
-}
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+client_script "client.js"
